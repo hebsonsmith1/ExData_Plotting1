93bb3917cf384652fe298cb18eba2e311e20597f
user <audreyhebsonsmith@gmail.com>
2014-05-10 23:34:46 -0500

    added png and code files

--- plot1.R
+++ plot1.R
@@ -9,7 +9,10 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 # subset data by dates needed
 byDate <- data[(data$Date > "2007-01-31" & data$Date < "2007-02-03"),]
 # initialize image
+byDate$Global_active_power <- as.numeric(as.character
+                                         (byDate$Global_active_power))
 png("plot1.png", 480, 480)
 # create histogram
 hist(byDate$Global_active_power, main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)", col = "red")
+dev.off()
